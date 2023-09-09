3dbdb3bda65084b41fde0195ede5f98b908ea550
Shaid1998 <shaidur35-2300@diu.edu.bd>
2023-09-09 21:50:58 +0600

    Home Top

--- src/Component/Home/HomeTop.jsx
+++ src/Component/Home/HomeTop.jsx
@@ -1,10 +1,19 @@
 import React, { Component, Fragment } from 'react'
+import VideoBackground from '../../Assets/Video/BackgroundTop.mp4'
+
 
 class HomeTop extends Component {
   render() {
     return (
       <Fragment>
-        <h1>Here is Home</h1>
+        <div className='TopMain' fluid>
+          
+          <video className='Video' src={VideoBackground} autoPlay loop muted/>
+          <div >
+            <h1 className='OverText'>1GYM</h1>
+            <h4 className='OverTextSub'>Welcome Here</h4>
+          </div>
+        </div>
       </Fragment>
     )
   }
