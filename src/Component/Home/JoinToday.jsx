5d5c57413f0a2a1421f7a52716fab2d7fd52809f
Shaid1998 <shaidur35-2300@diu.edu.bd>
2023-09-11 23:37:10 +0600

    Footer Component

--- src/Component/Home/JoinToday.jsx
+++ src/Component/Home/JoinToday.jsx
@@ -12,7 +12,7 @@ class JoinToday extends Component {
                     <button className='btn btn-primary ButtonSet' size="lg">JOIN YOUR JUARNEY</button>
                 </div>
                 <div>
-                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3692.291100011103!2d88.44166617488192!3d22.266956144103982!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a026b8946c67695%3A0x94978cd7b4e9d21c!2sBaruipur%20-%20Kulpi%20Rd%2C%20Sarberia%2C%20West%20Bengal%2C%20India!5e0!3m2!1sen!2sbd!4v1694432335920!5m2!1sen!2sbd" width="100%" height="450" style={{border:'0'}} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3692.291100011103!2d88.44166617488192!3d22.266956144103982!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a026b8946c67695%3A0x94978cd7b4e9d21c!2sBaruipur%20-%20Kulpi%20Rd%2C%20Sarberia%2C%20West%20Bengal%2C%20India!5e0!3m2!1sen!2sbd!4v1694432335920!5m2!1sen!2sbd" width="100%" height="350" style={{border:'0'}} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
                 </div>
                 </Container>
             </Fragment>
