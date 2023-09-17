b96a1ededf1259132349897b52df32d45a0817b8
Shaid1998 <shaidur35-2300@diu.edu.bd>
2023-09-17 14:20:14 +0600

    Training Component

--- src/Component/Training/Training.jsx
+++ src/Component/Training/Training.jsx
@@ -8,12 +8,13 @@ class Training extends Component {
       <Fragment>
         <Row className='R1'>
             <div className='TrTitle'>
-                <h1>TRAINING</h1>
+                <h1 className='TrHead'>TRAINING</h1>
             </div>
             <div className='TrDescription'>
-                <h4>Our certified personal trainers create the perfect programs to help you reach your goals, designed 100% around you.</h4>
+                <h4 className='DesTr'>Our certified personal trainers create the perfect programs to help you reach your goals, designed 100% around you.</h4>
             </div>
         </Row>
+        <hr className='LineH' />
         <Row className='R2'>
             <Col className='C1' lg={6} md={6} sm={6}>
                 <div>
@@ -22,15 +23,15 @@ class Training extends Component {
             </Col>
             <Col className='C2' lg={6} md={6} sm={6}>
                 <div className='PTrTitle'>
-                    <h1>1GYM</h1>
+                    <h1 className='TrTi'>1GYM</h1>
                 </div>
                 <div className='PTrDescription'>
-                    <p>Here at 1GYM, we focus on bringing you the most powerful workouts to meet your needs: our toolbox includes weighted battle ropes, kettlebells, TRX® suspension straps, Turf Zone, Med Balls, and the list goes on</p>
-                    <p>It may sound like a lot, but this world-class functional training equipment helps our trainers provide you with an array of High Intensity Interval Training (HIIT) workouts.</p>
-                    <p>Our FITNESS nGO locations offer PowerPlate, an all-encompassing fitness program that includes revolutionary vibration technology. Come check it out for yourself!</p>
+                    <p className='TrDes'>Here at 1GYM, we focus on bringing you the most powerful workouts to meet your needs: our toolbox includes weighted battle ropes, kettlebells, TRX® suspension straps, Turf Zone, Med Balls, and the list goes on</p>
+                    <p className='TrDes'>It may sound like a lot, but this world-class functional training equipment helps our trainers provide you with an array of High Intensity Interval Training (HIIT) workouts.</p>
+                    <p className='TrDes'>Our FITNESS nGO locations offer PowerPlate, an all-encompassing fitness program that includes revolutionary vibration technology. Come check it out for yourself!</p>
                 </div>
                 <div>
-                    <a className='PTrainBut'><Button href='/personaltraining' variant="success" size="huge">JOIN NOW</Button></a>
+                    <a className='PTrainBut'><Button href='/personaltraining' variant="success" className='TrBtn' size="huge">JOIN NOW</Button></a>
                 </div>
             </Col>
         </Row>
