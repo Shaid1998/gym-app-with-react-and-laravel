da370dfd7e3adaee2780631e357e7714b7bc5426
Shaid1998 <shaidur35-2300@diu.edu.bd>
2023-09-18 01:39:13 +0600

    Facility Component Design Facility top

--- src/Component/FACILITIES/FacTop.jsx
+++ src/Component/FACILITIES/FacTop.jsx
@@ -5,14 +5,14 @@ class FacTop extends Component {
   render() {
     return (
       <Fragment>
-        <div>
-          <Row>
-            <div>
-              <h1>$2 Million-Dollars in Renovations!</h1>
-              <h4>Come in and experience the best gym in the Port Saint Lucie area.</h4>
+        <div className='FTop'>
+          <Row className='FRO'>
+            <div className='Div1'>
+              <h1 className='TTTitle'>$2 Million-Dollars in Renovations!</h1>
+              <h4 className='TTSubTitle'>Come in and experience the best gym in the Port Saint Lucie area.</h4>
             </div>
-            <div>
-              <a className='PTrainButTop'><Button href='/personaltraining' className='PtrB' variant="success" size="huge">JOIN NOW</Button></a>
+            <div className='Div2'>
+              <a className='FacilitiesButTop'><Button href='/reg' className='FYB' variant="success" size="huge">JOIN NOW</Button></a>
             </div>
           </Row>
         </div>
