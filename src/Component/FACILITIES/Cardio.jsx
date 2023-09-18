95b27b463f4997b9708d73820139cbc0d8847d53
Shaid1998 <shaidur35-2300@diu.edu.bd>
2023-09-18 14:58:02 +0600

    Facilities Component Cardio Part

--- src/Component/FACILITIES/Cardio.jsx
+++ src/Component/FACILITIES/Cardio.jsx
@@ -6,16 +6,16 @@ class Cardio extends Component {
   render() {
     return (
       <Fragment>
-        <div className='Personal'> 
-          <Row className='PersonalTrain'>
-              <Col className='PersonalTrainColumn1' lg={6} md={6} sm={6}>
-                  <div className='titlediv'>
-                      <h1 className='MainTitile'>CARDIO</h1>
+        <div className='Cardio'> 
+          <Row className='CarRo'>
+              <Col className='CCOL1' lg={6} md={6} sm={6}>
+                  <div className='CATitle'>
+                      <h1 className='CaTiTle'>CARDIO</h1>
                   </div>
-                  <p className='MainDescription'>Get your heart and lungs working with our brand new cardio.  All of them have personal fans to keep you cool and dozens of 72" 4k televisions for your entertainment.  We have them all: Treadmills, stairmills, upright/recumbent/spin bikes, rowers, ellipticals.</p>
+                  <p className='CaDes'>Get your heart and lungs working with our brand new cardio.  All of them have personal fans to keep you cool and dozens of 72" 4k televisions for your entertainment.  We have them all: Treadmills, stairmills, upright/recumbent/spin bikes, rowers, ellipticals.</p>
               </Col>
-              <Col className='PersonalTrainColumn2' lg={6} md={6} sm={6}>
-                  <img className='personalImage'  src={CImg} />
+              <Col className='CCOL2' lg={6} md={6} sm={6}>
+                  <img className='CaIMg'  src={CImg} />
               </Col>
           </Row>
         </div>
