073f12ba5613c9262ea01e41b617dc5c1f00e775
Shaid1998 <shaidur35-2300@diu.edu.bd>
2023-09-18 22:31:44 +0600

    Anemities Component Tanning Part

--- src/Component/AMENITIES/Tanning.jsx
+++ src/Component/AMENITIES/Tanning.jsx
@@ -1,9 +1,27 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
+import { Col, Row } from 'react-bootstrap'
+import TVideo from '../../Assets/Video/Tanning.mp4'
 
 class Tanning extends Component {
   render() {
     return (
-      <div>Tanning</div>
+      <Fragment>
+        <div className='Tanning'>
+          <Row className='Recover1G'>
+            <Col className='REco1' lg={6} md={6} sm={6}>
+              <video className='KidVideoClass' src={TVideo} autoPlay loop muted />
+            </Col>
+            <Col className='REco2' lg={6} md={6} sm={6}>
+              <div className='RecoT'>
+                  <h1 className='RTi'>Recover nGo</h1>
+              </div>
+              <div className='RecoDescription'>
+                  <p className='RecoDes'>By laying down 10 to 20 minutes in our latest medical breakthrough CRYOLOUNGE+. HumanTouch, or HYDROMASSAGE chairs you will-Ease your body of pain, discomfort, and aches - Relax away the stresses of your daily rat race & Prepare your muscles and mind for your next workout. </p>
+              </div>
+            </Col>
+          </Row>
+        </div>
+      </Fragment>
     )
   }
 }
