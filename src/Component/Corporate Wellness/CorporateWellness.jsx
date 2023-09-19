d8000ed2bd0f1187e025e2b4672ed48979f46a59
Shaid1998 <shaidur35-2300@diu.edu.bd>
2023-09-20 00:55:16 +0600

    Corporate Wellness Component Some Part

--- src/Component/Corporate Wellness/CorporateWellness.jsx
+++ src/Component/Corporate Wellness/CorporateWellness.jsx
@@ -1,9 +1,13 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
+import { Col, Row } from 'react-bootstrap'
+import CWImg from '../../Assets/Image/CustomerService/Wellness.jpg'
 
 class CorporateWellness extends Component {
   render() {
     return (
-      <div>CorporateWellness</div>
+      <Fragment>
+        
+      </Fragment>
     )
   }
 }
--- src/Component/Corporate Wellness/SomeWord.jsx
+++ src/Component/Corporate Wellness/SomeWord.jsx
@@ -1,9 +1,29 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
+import { Col, Row } from 'react-bootstrap'
+import WImg from '../../Assets/Image/CustomerService/Some.jpg'
 
 class SomeWord extends Component {
   render() {
     return (
-      <div>SomeWord</div>
+      <Fragment>
+        <div className='WellnessSome'>
+          <Row className='WellnessRoW'>
+            <Col className='WellnessCOl1' lg={6} md={6} sm={6}>
+              <div>
+                <img className='WellnessImgTF'  src={WImg} />
+              </div>
+            </Col>
+            <Col className='WellnessCol2' lg={6} md={6} sm={6}>
+              <div className='WellnessFloorT'>
+                  <h1 className='WellnessTFTi'>Training Floor</h1>
+              </div>
+              <div className='WellnessTFDescription'>
+                  <p className='WellnessTFDes'>Our fully renovated 25,000 sq.ft. gym provides you all the space you need to enjoy your pursuit of a healthier and happier you! We offer all the brand names from: MATRIX, NAUTILUS, HAMMER, STAIRMASTER, LIFE FITNESS, TORQUE, and more.</p>
+              </div>
+            </Col>
+          </Row>
+        </div>
+      </Fragment>
     )
   }
 }
