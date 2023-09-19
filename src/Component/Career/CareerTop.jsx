4ac76ed16c052d07efff1af1b3b147dab9355ca5
Shaid1998 <shaidur35-2300@diu.edu.bd>
2023-09-19 12:57:00 +0600

    Career Part Career Top Component Design

--- src/Component/Career/CareerTop.jsx
+++ src/Component/Career/CareerTop.jsx
@@ -1,9 +1,17 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
+import { Row } from 'react-bootstrap'
 
 class CareerTop extends Component {
   render() {
     return (
-      <div>CareerTop</div>
+      <Fragment>
+        <Row className='CTop'>
+          <div className='DivRow'>
+            <h1 className='CarTitle'>CAREERS</h1>
+            <h6 className='CarSubTitle'>Work At The Best Gym In Port Saint Lucie</h6>
+        </div>
+        </Row>
+      </Fragment>
     )
   }
 }
