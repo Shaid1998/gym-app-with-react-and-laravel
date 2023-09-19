03a7a37fb301aa9416c72a2744aeb26a52cbd625
Shaid1998 <shaidur35-2300@diu.edu.bd>
2023-09-20 00:59:35 +0600

    Corporate Wellness Component Some Part

--- src/Component/Corporate Wellness/CorporateWellness.jsx
+++ src/Component/Corporate Wellness/CorporateWellness.jsx
@@ -6,7 +6,19 @@ class CorporateWellness extends Component {
   render() {
     return (
       <Fragment>
-        
+        <div className='CorporateWellness'> 
+          <Row className='CorporateCarRo'>
+              <Col className='CorporateCCOL1' lg={6} md={6} sm={6}>
+                  <div className='CorporateCATitle'>
+                      <h1 className='CorporateCaTiTle'>CARDIO</h1>
+                  </div>
+                  <p className='CorporateCaDes'>Get your heart and lungs working with our brand new cardio.  All of them have personal fans to keep you cool and dozens of 72" 4k televisions for your entertainment.  We have them all: Treadmills, stairmills, upright/recumbent/spin bikes, rowers, ellipticals.</p>
+              </Col>
+              <Col className='CorporateCCOL2' lg={6} md={6} sm={6}>
+                  <img className='CorporateCaIMg'  src={CWImg} />
+              </Col>
+          </Row>
+        </div>
       </Fragment>
     )
   }
