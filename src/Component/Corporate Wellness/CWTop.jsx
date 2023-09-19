637170fa47615936b47811b6029194bbc4265824
Shaid1998 <shaidur35-2300@diu.edu.bd>
2023-09-20 00:36:14 +0600

    Corporate Wellness Component Top Part

--- src/Component/Corporate Wellness/CWTop.jsx
+++ src/Component/Corporate Wellness/CWTop.jsx
@@ -1,9 +1,17 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
+import { Row } from 'react-bootstrap'
 
 class CWTop extends Component {
   render() {
     return (
-      <div>CWTop</div>
+      <Fragment>
+        <div className='CWTop'>
+          <Row className='CWTopR'>
+            <h1 className='CWTTitle'>CORPORATE WELLNESS PROGRAMS</h1>
+            <h6 className='CWTSubTitle'>Provide A Healthy Employee Opportunity At The Best Gym In Port Saint Lucie</h6>
+          </Row>
+        </div>
+      </Fragment>
     )
   }
 }
