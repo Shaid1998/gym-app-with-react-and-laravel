6adda306cfa9ad2fde50b9a94ce1c6c96ede6406
Shaid1998 <shaidur35-2300@diu.edu.bd>
2023-09-19 16:01:42 +0600

    Customer Service Design

--- src/Component/Customer Service/CustomerService.jsx
+++ src/Component/Customer Service/CustomerService.jsx
@@ -1,9 +1,28 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
+import { Row , Col, Button } from 'react-bootstrap'
 
 class CustomerService extends Component {
   render() {
     return (
-      <div>Customer Service</div>
+      <Fragment>
+        <div>
+          <Row>
+            <h1>How Can We Help?</h1>
+          </Row>
+          <Row>
+            <Col lg={6} md={6} sm={6}>
+              <a><i class="fas fa-dumbbell"></i></a>
+              <p>Please visit our FAQ page for commonly asked questions.</p>
+              <a><Button href='#'  variant="success">FAQ</Button></a>
+            </Col>
+            <Col lg={6} md={6} sm={6}>
+              <a><i class="fas fa-envelope"></i></a>
+              <p>For billing questions, cancellation requests, or complaints and compliments: Email us at service@1gym.com</p>
+              <a><Button href='#' variant="success">Contact</Button></a>
+            </Col>
+          </Row>
+        </div>
+      </Fragment>
     )
   }
 }
