41cbbf05e5fc744a6d435524fb59cbac4cb7e97a
Shaid1998 <shaidur35-2300@diu.edu.bd>
2023-09-19 16:56:05 +0600

    FAQ Component Design pattern

--- src/Component/FAQ/FAQ.jsx
+++ src/Component/FAQ/FAQ.jsx
@@ -1,9 +1,71 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
+import { Col, Row } from 'react-bootstrap'
 
 class FAQ extends Component {
   render() {
     return (
-      <div>FAQ</div>
+      <Fragment>
+        <div>
+            <Row>
+                <h1>Frequently Asked Questions</h1>
+            </Row>
+            <Row>
+                <Col lg={6} md={6} sm={6}>
+                    <Row>
+                        <Col lg={6} md={6} sm={6}>
+                            <h1>Logo</h1>
+                        </Col>
+                        <Col lg={6} md={6} sm={6}>
+                            <h1>Description</h1>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col lg={6} md={6} sm={6}>
+                            <h1>Logo</h1>
+                        </Col>
+                        <Col lg={6} md={6} sm={6}>
+                            <h1>Description</h1>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col lg={6} md={6} sm={6}>
+                            <h1>Logo</h1>
+                        </Col>
+                        <Col lg={6} md={6} sm={6}>
+                            <h1>Description</h1>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col lg={6} md={6} sm={6}>
+                            <h1>Logo</h1>
+                        </Col>
+                        <Col lg={6} md={6} sm={6}>
+                            <h1>Description</h1>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col lg={6} md={6} sm={6}>
+                            <h1>Logo</h1>
+                        </Col>
+                        <Col lg={6} md={6} sm={6}>
+                            <h1>Description</h1>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col lg={6} md={6} sm={6}>
+                            <h1>Logo</h1>
+                        </Col>
+                        <Col lg={6} md={6} sm={6}>
+                            <h1>Description</h1>
+                        </Col>
+                    </Row>
+                </Col>
+                <Col lg={6} md={6} sm={6}>
+                    <h1>Img</h1>
+                </Col>
+            </Row>
+        </div>
+      </Fragment>
     )
   }
 }
