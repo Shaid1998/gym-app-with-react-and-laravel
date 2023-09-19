b14336d4af2d0a09f474f0798a02c9964e78f13b
Shaid1998 <shaidur35-2300@diu.edu.bd>
2023-09-19 13:49:36 +0600

    Career Part Welcome Desk Component

--- src/Component/Career/KidsClubCareer.jsx
+++ src/Component/Career/KidsClubCareer.jsx
@@ -10,7 +10,7 @@ class KidsClubCareer extends Component {
           <Row className='KidsCarRoW'>
             <Col className='KidsCarCol1' lg={6} md={6} sm={6}>
               <div className='KidsCarT'>
-                  <h1 className='PerCarTitile'>KIDS CLUB</h1>
+                  <h1 className='KidsCarTitile'>KIDS CLUB</h1>
               </div>
               <div className='KidsCarDescription'>
                   <p className='KidsCarDes'>If you love kids this is the role for you!  We want our members to know that they can enjoy their workouts while their kids are being cared for by our team.  Earning Potential: starting at $12 per hour</p>
@@ -21,7 +21,6 @@ class KidsClubCareer extends Component {
                 <img className='KidsCarImg'  src={KidImg} />
               </div>
             </Col>
-            
           </Row>
         </div>
       </Fragment>
--- src/Component/Career/WelcomeDesk.jsx
+++ src/Component/Career/WelcomeDesk.jsx
@@ -1,9 +1,29 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
+import { Col, Row } from 'react-bootstrap'
+import WelcomeDeskImg from '../../Assets/Image/Careers/Welcome.jpg'
 
 class WelcomeDesk extends Component {
   render() {
     return (
-      <div>WelcomeDesk</div>
+      <Fragment>
+        <div className='WelcomeDeskCareer'>
+          <Row className='WelcomeCarRoW'>
+            <Col className='WelcomeCarCol1' lg={6} md={6} sm={6}>
+              <div className='WelcomeCarT'>
+                  <h1 className='WelcomeCarTitile'>KIDS CLUB</h1>
+              </div>
+              <div className='WelcomeCarDescription'>
+                  <p className='WelcomeCarDes'>If you love kids this is the role for you!  We want our members to know that they can enjoy their workouts while their kids are being cared for by our team.  Earning Potential: starting at $12 per hour</p>
+              </div>
+            </Col>
+            <Col className='WelcomeCarCol2' lg={6} md={6} sm={6}>
+              <div>
+                <img className='WelcomeCarImg'  src={WelcomeDeskImg} />
+              </div>
+            </Col>
+          </Row>
+        </div>
+      </Fragment>
     )
   }
 }
