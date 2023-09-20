38409014d0d412f4769a6711d2ba7748f238a7c9
Shaid1998 <shaidur35-2300@diu.edu.bd>
2023-09-20 14:46:23 +0600

    Privacy And Policy Part Copy Of privacy Component

--- src/Component/Privacy/CopuOfPrivacy.jsx
+++ src/Component/Privacy/CopuOfPrivacy.jsx
@@ -1,9 +1,39 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
 
 class CopuOfPrivacy extends Component {
   render() {
     return (
-      <div>CopuOfPrivacy</div>
+      <Fragment>
+        <div>
+            <h1>Last Updated:</h1>
+            <p> 08/03/2023. PLEASE READ THE FOLLOWING TERMS OF SERVICE AGREEMENT CAREFULLY. BY ACCESSING OR USING OUR WEBSITE (THE "SITE") OR OUR SERVICES, YOU HEREBY AGREE TO BE BOUND BY THESE TERMS AND CONDITIONS AND ALL TERMS INCORPORATED HEREIN BY REFERENCE. IT IS THE RESPONSIBILITY OF YOU, THE USER, CUSTOMER, OR PROSPECTIVE CUSTOMER TO READ THE TERMS AND CONDITIONS BEFORE PROCEEDING TO USE THIS SITE. IF YOU DO NOT EXPRESSLY AGREE TO ALL OF THE TERMS AND CONDITIONS, THEN PLEASE DO NOT ACCESS OR USE OUR SITE OR OUR SERVICES.</p>
+            <p> The present terms and conditions (this "Agreement" or "Terms") is a legal agreement between you and FITNESS NGO 1 LLC (hereinafter " FITNESSnGO"), a company duly organized and validly existing, located at 250 NW Peacock Blvd, Port Saint Lucie, Florida 34986. This Agreement annuls and voids all previous agreements.</p>
+
+            <h1>OVERVIEW</h1>
+            <p> The Site (www.fitnessngo.com) is operated by FITNESSnGO. Throughout the Site, the terms "we", "us" and "our" refer to FITNESSnGO. FITNESSnGO offers this Site, including all information, tools and services available from this Site to you, the user, conditioned upon your acceptance of all terms, conditions, policies and notices stated here. By visiting our Site and/or purchasing something from us, you engage in our "Service" and agree to be bound by the following terms and conditions, including those additional terms and conditions and policies referenced herein and/or available by hyperlink. These Terms apply to all users of the Site, including without limitation users who are browsers, vendors, customers, merchants, and/or contributors of content. In the event of an inconsistency between this Agreement and any additional terms or policies referenced herein, the provisions of the additional terms or policies shall control. Please read these Terms carefully before accessing or using our Site.</p>
+            <p> By accessing or using any part of the Site, you agree to be bound by these Terms. If you do not agree to all the Terms of this Agreement, then you may not access the Site or use any Service. If these Terms are considered an offer, acceptance is expressly limited to these Terms. Any new features or tools which are added to the current store shall also be subject to the Terms. You can review the most current version of the Terms at any time on this page. We reserve the right to update, change or replace any part of these Terms by posting updates and/or changes to our Site. It is your responsibility to check this page periodically for changes. Your continued use of or access to the Site following the posting of any changes constitutes acceptance of those changes.</p>
+
+            <h4>SECTION 1 - GENERAL TERMS</h4>
+            <h6>By agreeing to these Terms, you represent that you are at least the age of majority in your state or province of residence, or that you are the age of majority in your state or province of residence and you have given us your consent to allow any of your minor dependents to use this Site. You may not use our products or Site for any illegal or unauthorized purpose nor may you, in the use of our products or Site, violate any laws in your jurisdiction (including but not limited to motor vehicle laws). You must not transmit any worms or viruses or any code of a destructive nature. A breach or violation of any of the Terms will result in an immediate termination of your account and right to use our Service. We have the right, but not the obligation, to take any of the following actions in our sole discretion at any time and for any reason without giving you any prior notice:</h6>
+            <p>You agree that we will not be liable to you or any third party for taking any of these actions.</p>
+            <p>You understand and agree that our Site may include communications such as service announcements and administrative or legal notices from us. Please note that you cannot opt out of receiving these notices.</p>
+            <p>You understand that your content (not including credit card information), may be transferred unencrypted and involve (a) transmissions over various networks; and (b) changes to conform and adapt to technical requirements of connecting networks or devices. Credit card information is always encrypted during transfer over networks.</p>
+            <p>You agree not to reproduce, duplicate, copy, sell, resell or exploit any portion of the Site, use of the Site, or access to the Site or any contact on the Site, without express written permission by us.</p>
+            <p>You may not modify, publish, transmit, reverse engineer, participate in the transfer or sale, create derivative works, or in any way exploit any of the content, in whole or in part, found on the Site. FITNESSnGO content is not for resale. Use of the Site does not entitle users to make any unauthorized use of any protected content, and in particular you will not delete or alter any proprietary rights or attribution notices in any content. You will use protected content solely for your personal use, and will make no other use of the content without the express written permission of FITNESSnGO and the copyright owner. You agree that you do not acquire any ownership rights in any protected content. We do not grant you any licenses, express or implied, to the intellectual property of FITNESSnGO or our licensors except as expressly authorized by these Terms.</p>
+            <h4>SECTION 2 - CREATING AN ACCOUNT</h4>
+            <p>Once you create an account with us, you are registered on the FITNESSnGO Site. The terms "member," "membership," and "account" all refer to this registration as a member on FITNESSnGO's Site. If you are merely surfing or browsing through the Site and have not yet created an account, your use of the Site is still subject to this Agreement; if you do not agree to this Agreement, do not use the Site.</p>
+            <p>When you create an account, you will provide a unique username and email. We will also ask you to create a password. Because any activities that occur under your username or password are your responsibility it is important for you to keep your username and/or password secure. You may not assign or otherwise transfer your account to any other person or entity. You acknowledge that FITNESSnGO is not responsible for third party access to your account that results from theft or misappropriation of your account. Notify us immediately if you believe that someone has used your username, email, or password without your authorization.</p>
+            <h6>Furthermore, the registering party hereby acknowledges, understands and agrees to:</h6>
+            <p>You agree that we will not be liable to you or any third party for taking any of these actions.</p>
+            <p>You agree that we will not be liable to you or any third party for taking any of these actions.</p>
+            <p>You agree that we will not be liable to you or any third party for taking any of these actions.</p>
+            <p>You agree that we will not be liable to you or any third party for taking any of these actions.</p>
+            <p>You agree that we will not be liable to you or any third party for taking any of these actions.</p>
+            <p>You agree that we will not be liable to you or any third party for taking any of these actions.</p>
+            <p>You agree that we will not be liable to you or any third party for taking any of these actions.</p>
+            <p>You agree that we will not be liable to you or any third party for taking any of these actions.</p>
+        </div>
+      </Fragment>
     )
   }
 }
