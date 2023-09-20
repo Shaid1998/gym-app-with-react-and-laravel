8fbd09650bea98a0cd87f0f4746fa2baf431a7d5
Shaid1998 <shaidur35-2300@diu.edu.bd>
2023-09-20 14:16:17 +0600

    Corporate Wellness Component Componenet Wellness part mailto correction

--- src/Component/Corporate Wellness/CorporateWellness.jsx
+++ src/Component/Corporate Wellness/CorporateWellness.jsx
@@ -13,7 +13,7 @@ class CorporateWellness extends Component {
                       <h1 className='CorporateCaTiTle'>CORPORATE WELLNESS PROGRAMS ARE  CRITICAL FOR COMPANIES</h1>
                   </div>
                   <p className='CorporateCaDes'>Give your employees the luxury of being part of the best gym in Port Saint Lucie. Not only will they be healthier, but they will be happier too.. Which means more productivity!</p>
-                  <Button href="operationspsl@fitnessngo.com" className='CorporateButton2' variant="success">INQUIRE</Button>
+                  <Button href="mailto:operationspsl@1gym.com" className='CorporateButton2' variant="success">INQUIRE</Button>
               </Col>
               <Col className='CorporateCCOL2' lg={6} md={6} sm={6}>
                   <img className='CorporateCaIMg'  src={CWImg} />
