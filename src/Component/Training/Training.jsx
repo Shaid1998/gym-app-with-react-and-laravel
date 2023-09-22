f862e4d2ff822489bc6d97b0cbce3e20a255f1bc
Shaid1998 <shaidur35-2300@diu.edu.bd>
2023-09-22 13:59:34 +0600

    Training Data Consumed

--- src/Component/Training/Training.jsx
+++ src/Component/Training/Training.jsx
@@ -1,34 +1,52 @@
 import React, { Component, Fragment } from 'react'
 import { Button, Col, Row } from 'react-bootstrap'
 import PTrain from '../../Assets/Image/Training/ptrain.jpg'
+import BaseUrl from '../../BaseUrl/BaseUrl';
+import RestClient from '../../BaseUrl/RestClient';
 
 class Training extends Component {
+    constructor(){
+        super();
+        this.state={
+            training_top_title:'.......',
+            training_top_sub_title:'.......',
+            taining_title:'.......',
+            taining_details:'.......',
+            training_image:'.......'
+        }
+      }
+      componentDidMount(){
+        RestClient.GetRequest(BaseUrl.AllTrainingData).then(result=>{
+            this.setState({training_top_title:result[0]['training_top_title'],training_top_sub_title:result[0]['training_top_sub_title'],taining_title:result[0]['taining_title'],taining_details:result[0]['taining_details'],training_image:result[0]['training_image']});
+        }).catch(error=>{
+          this.setState({training_top_title:'???',training_top_sub_title:'???',taining_title:'???',taining_details:'???',training_image:'???'})
+        })
+      }
   render() {
     return (
       <Fragment>
         <Row className='R1'>
             <div className='TrTitle'>
-                <h1 className='TrHead'>TRAINING</h1>
+                <h1 className='TrHead'>{this.state.training_top_title}</h1>
             </div>
             <div className='TrDescription'>
-                <h4 className='DesTr'>Our certified personal trainers create the perfect programs to help you reach your goals, designed 100% around you.</h4>
+                <h4 className='DesTr'>{this.state.training_top_sub_title}</h4>
             </div>
         </Row>
         <hr className='LineH' />
         <Row className='R2'>
-            <Col className='C1' lg={6} md={6} sm={6}>
+            <Col className='C1254' lg={6} md={6} sm={6}>
                 <div>
                     <img className='pTIMG' src={PTrain} />
                 </div>
             </Col>
-            <Col className='C2' lg={6} md={6} sm={6}>
+            <Col className='C1252' lg={6} md={6} sm={6}>
                 <div className='PTrTitle'>
                     <h1 className='TrTi'>1GYM</h1>
                 </div>
                 <div className='PTrDescription'>
-                    <p className='TrDes'>Here at 1GYM, we focus on bringing you the most powerful workouts to meet your needs: our toolbox includes weighted battle ropes, kettlebells, TRX® suspension straps, Turf Zone, Med Balls, and the list goes on</p>
-                    <p className='TrDes'>It may sound like a lot, but this world-class functional training equipment helps our trainers provide you with an array of High Intensity Interval Training (HIIT) workouts.</p>
-                    <p className='TrDes'>Our FITNESS nGO locations offer PowerPlate, an all-encompassing fitness program that includes revolutionary vibration technology. Come check it out for yourself!</p>
+                    <p className='TrDes'>{this.state.taining_details}</p>
+                    
                 </div>
                 <div>
                     <a className='PTrainBut'><Button href='/personaltraining' variant="success" className='TrBtn' size="huge">JOIN NOW</Button></a>
