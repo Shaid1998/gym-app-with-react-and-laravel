ff04e7d7ac54403da0836cddb08e3e1b1d6f8f65
Shaid1998 <shaidur35-2300@diu.edu.bd>
2023-09-23 07:03:58 +0600

    Previous Correction

--- src/Component/Home/WhyUs.jsx
+++ src/Component/Home/WhyUs.jsx
@@ -19,14 +19,14 @@ class WhyUs extends Component {
                                 <h1 className='WhyMainTitile'>WHY 1GM ?</h1>
                                 <h5 className='WHYMainDescription'>Luxury Gym Experience Everyone Can Afford.</h5>
                             </div>
-                            <div className='whymenudiv'>
-                               <h4 className='WhyMenu'><i class="fa fa-check checkSet" aria-hidden="true"></i>Super Clean</h4> 
-                               <h4 className='WhyMenu'><i class="fa fa-check checkSet" aria-hidden="true"></i>Friendly Staff</h4> 
-                               <h4 className='WhyMenu'><i class="fa fa-check checkSet" aria-hidden="true"></i>Workout & Recover</h4> 
-                               <h4 className='WhyMenu'><i class="fa fa-check checkSet" aria-hidden="true"></i>Motivating Atmosphere</h4> 
+                            <div className='whymenudiv1'>
+                               <h4 className='WhyMenu1'><i class="fa fa-check checkSet1" aria-hidden="true"></i>Super Clean</h4> 
+                               <h4 className='WhyMenu1'><i class="fa fa-check checkSet1" aria-hidden="true"></i>Friendly Staff</h4> 
+                               <h4 className='WhyMenu1'><i class="fa fa-check checkSet1" aria-hidden="true"></i>Workout & Recover</h4> 
+                               <h4 className='WhyMenu1'><i class="fa fa-check checkSet1" aria-hidden="true"></i>Motivating Atmosphere</h4> 
                             </div>
-                            <div className='buttonClass'>
-                                <Button className='btn btn-primary JoinButton'>START YOUR JOURNEY</Button>
+                            <div className='buttonClass1'>
+                                <Button className='btn btn-primary JoinButton1'>START YOUR JOURNEY</Button>
                             </div>
                         </Col>
                     </Row>
