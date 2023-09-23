8681c01969d16fee393504081970c6672d12863c
Shaid1998 <shaidur35-2300@diu.edu.bd>
2023-09-23 15:31:36 +0600

    Facilities Component Dynamic and api consume

--- Backend/database/migrations/2023_09_23_092651_create_facilities_table.php
+++ Backend/database/migrations/2023_09_23_092651_create_facilities_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('facilities', function (Blueprint $table) {
             $table->id();
+            $table->string('facilities_title')->nullable();
+            $table->text('facilities_Sub_title')->nullable();
+            $table->string('facilities_image')->nullable();
             $table->timestamps();
         });
     }
