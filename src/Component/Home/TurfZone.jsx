78c294ab2279d5d7f147fb279619afb2b92dd0f3
Shaid1998 <shaidur35-2300@diu.edu.bd>
2023-09-23 08:57:12 +0600

    Turf Zone Component Dynamic and Data consuming

--- src/Component/Home/TurfZone.jsx
+++ src/Component/Home/TurfZone.jsx
@@ -2,8 +2,27 @@ import React, { Component, Fragment } from 'react'
 import { Col, Container, Row } from 'react-bootstrap'
 import Img1 from '../../Assets/Image/PSlider6.jpg'
 import Img2 from '../../Assets/Image/PSlider3.jpg'
+import BaseUrl from '../../BaseUrl/BaseUrl'
+import RestClient from '../../BaseUrl/RestClient'
 
 class TurfZone extends Component {
+    constructor(){
+        super();
+        this.state={
+            turfzone_title_1:'loading data.......',
+            turfzone_title_2:'loading data.......',
+            turfzone_title_3:'loading data.......',
+            turfzone_description:'loading data.......',
+            turfzone_image:'loading data.......'
+        }
+      }
+      componentDidMount(){
+        RestClient.GetRequest(BaseUrl.AllTurfZoneDataShow).then(result=>{
+            this.setState({turfzone_title_1:result[0]['turfzone_title_1'],turfzone_title_2:result[0]['turfzone_title_2'],turfzone_title_3:result[0]['turfzone_title_3'],turfzone_description:result[0]['turfzone_description'],turfzone_image:result[0]['turfzone_image']});
+        }).catch(error=>{
+          this.setState({turfzone_title_1:'???',turfzone_title_2:'???',turfzone_title_3:'???',turfzone_description:'???',turfzone_image:'???'})
+        })
+      }
     render() {
         return (
             <Fragment> 
@@ -12,11 +31,11 @@ class TurfZone extends Component {
                     <Row className='TurfZone'>
                         <Col className='TurfColumn1' lg={6} md={6} sm={6}>
                             <div className='ExpTitlediv'>
-                                <h1 className='TurfTitile'>THE</h1>
-                                <h1 className='TurfTitile2'>TURF</h1>
-                                <h1 className='TurfTitile3'>ZONE</h1>
+                                <h1 className='TurfTitile'>{this.state.turfzone_title_1}</h1>
+                                <h1 className='TurfTitile2'>{this.state.turfzone_title_2}</h1>
+                                <h1 className='TurfTitile3'>{this.state.turfzone_title_3}</h1>
                             </div>
-                            <p className='TurfMainDes'>The perfect place to knock out those athletic workouts, take content, or use the TANK.</p>
+                            <p className='TurfMainDes'>{this.state.turfzone_description}</p>
                         </Col> 
                         <Col className='TurfColumn2' lg={6} md={6} sm={6}>
                             <Row className='MIddleRow'>
