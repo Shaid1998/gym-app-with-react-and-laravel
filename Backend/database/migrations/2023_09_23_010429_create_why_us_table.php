edc0a10fd58b72bf6059a312272260769a568937
Shaid1998 <shaidur35-2300@diu.edu.bd>
2023-09-23 07:09:37 +0600

    Why Us Component Dynamic and Data consuming

---
+++ Backend/database/migrations/2023_09_23_010429_create_why_us_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('why_us', function (Blueprint $table) {
+            $table->id();
+            $table->id('why_us_title')->nullable();
+            $table->id('why_us_sub_title')->nullable();
+            $table->id('why_us_option_1')->nullable();
+            $table->id('why_us_option_2')->nullable();
+            $table->id('why_us_option_3')->nullable();
+            $table->id('why_us_option_4')->nullable();
+            $table->id('why_us_option_5')->nullable();
+            $table->id('why_us_option_6')->nullable();
+            $table->id('why_us_image')->nullable();
+            $table->id('why_us_video')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('why_us');
+    }
+};
