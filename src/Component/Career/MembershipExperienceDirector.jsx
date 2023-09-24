58af0a3462c57d83a34b74420c38916e669d27a0
Shaid1998 <shaidur35-2300@diu.edu.bd>
2023-09-24 17:21:48 +0600

    Footer Career Component Dunamic and Api Consume Director Career Part done

--- src/Component/Career/MembershipExperienceDirector.jsx
+++ src/Component/Career/MembershipExperienceDirector.jsx
@@ -1,8 +1,25 @@
 import React, { Component, Fragment } from 'react'
 import { Col, Row } from 'react-bootstrap'
 import DirectingImg from '../../Assets/Image/Careers/Director.jpg'
+import BaseUrl from '../../BaseUrl/BaseUrl'
+import RestClient from '../../BaseUrl/RestClient'
 
 class MembershipExperienceDirector extends Component {
+  constructor(){
+    super();
+    this.state={
+      career_job_title_6:'loading data.......',
+      career_job_description_6:'loading data.......',
+      career_job_image_6:'loading data.......'
+    }
+  }
+  componentDidMount(){
+    RestClient.GetRequest(BaseUrl.AllCareerDataShow).then(result=>{
+        this.setState({career_job_title_6:result[0]['career_job_title_6'],career_job_description_6:result[0]['career_job_description_6'],career_job_image_6:result[0]['career_job_image_6']});
+    }).catch(error=>{
+      this.setState({career_job_title_6:'???',career_job_description_6:'???',career_job_image_6:'???'})
+    })
+  }
   render() {
     return (
       <Fragment>
@@ -10,10 +27,10 @@ class MembershipExperienceDirector extends Component {
           <Row className='DirectorCarRoW'>
             <Col className='DirectorCarCol1' lg={6} md={6} sm={6}>
               <div className='DirectorCarT'>
-                  <h1 className='DirectorCarTitile'>WELCOME DESK</h1>
+                  <h1 className='DirectorCarTitile'>{this.state.career_job_title_6}</h1>
               </div>
               <div className='DirectorCarDescription'>
-                  <p className='DirectorCarDes'>If you have a big smile and positive energy, then we want you to be the first person that every member and guest sees when they enter our gym.  Your priority is to make everyone feel welcome and special as they come and go.  Earning Potential:  starting at $12 per hour</p>
+                  <p className='DirectorCarDes'>{this.state.career_job_description_6}</p>
               </div>
             </Col>
             <Col className='DirectorCarCol2' lg={6} md={6} sm={6}>
