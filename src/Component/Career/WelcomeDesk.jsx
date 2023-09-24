dfd83ab5a91370341dfa8f97fc42810b71a95d58
Shaid1998 <shaidur35-2300@diu.edu.bd>
2023-09-24 17:13:01 +0600

    Footer Career Component Dunamic and Api Consume Welcome Desk Career Part done

--- src/Component/Career/WelcomeDesk.jsx
+++ src/Component/Career/WelcomeDesk.jsx
@@ -1,8 +1,25 @@
 import React, { Component, Fragment } from 'react'
 import { Col, Row } from 'react-bootstrap'
 import WelcomeDeskImg from '../../Assets/Image/Careers/Welcome.jpg'
+import BaseUrl from '../../BaseUrl/BaseUrl'
+import RestClient from '../../BaseUrl/RestClient'
 
 class WelcomeDesk extends Component {
+  constructor(){
+    super();
+    this.state={
+      career_job_title_4:'loading data.......',
+      career_job_description_4:'loading data.......',
+      career_job_image_4:'loading data.......'
+    }
+  }
+  componentDidMount(){
+    RestClient.GetRequest(BaseUrl.AllCareerDataShow).then(result=>{
+        this.setState({career_job_title_4:result[0]['career_job_title_4'],career_job_description_4:result[0]['career_job_description_4'],career_job_image_4:result[0]['career_job_image_4']});
+    }).catch(error=>{
+      this.setState({career_job_title_4:'???',career_job_description_4:'???',career_job_image_4:'???'})
+    })
+  }
   render() {
     return (
       <Fragment>
@@ -10,10 +27,10 @@ class WelcomeDesk extends Component {
           <Row className='WelcomeCarRoW'>
             <Col className='WelcomeCarCol1' lg={6} md={6} sm={6}>
               <div className='WelcomeCarT'>
-                  <h1 className='WelcomeCarTitile'>WELCOME DESK</h1>
+                  <h1 className='WelcomeCarTitile'>{this.state.career_job_title_4}</h1>
               </div>
               <div className='WelcomeCarDescription'>
-                  <p className='WelcomeCarDes'>If you have a big smile and positive energy, then we want you to be the first person that every member and guest sees when they enter our gym.  Your priority is to make everyone feel welcome and special as they come and go.  Earning Potential:  starting at $12 per hour</p>
+                  <p className='WelcomeCarDes'>{this.state.career_job_description_4}</p>
               </div>
             </Col>
             <Col className='WelcomeCarCol2' lg={6} md={6} sm={6}>
