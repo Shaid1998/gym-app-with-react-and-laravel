6dbe2c723daf746ca042132382294d14803579d9
Shaid1998 <shaidur35-2300@diu.edu.bd>
2023-09-24 17:00:55 +0600

    Footer Career Component Dunamic and Api Consume  Personal Training Part done

--- src/Component/Career/PersonalTrainingCareer.jsx
+++ src/Component/Career/PersonalTrainingCareer.jsx
@@ -1,8 +1,25 @@
 import React, { Component, Fragment } from 'react'
 import { Col, Row } from 'react-bootstrap'
 import PersonalTImg from '../../Assets/Image/Careers/Trainer.jpg'
+import BaseUrl from '../../BaseUrl/BaseUrl'
+import RestClient from '../../BaseUrl/RestClient'
 
 class PersonalTrainingCareer extends Component {
+  constructor(){
+    super();
+    this.state={
+      career_job_title_1:'loading data.......',
+      career_job_description_1:'loading data.......',
+      career_job_image_1:'loading data.......'
+    }
+  }
+  componentDidMount(){
+    RestClient.GetRequest(BaseUrl.AllCareerDataShow).then(result=>{
+        this.setState({career_job_title_1:result[0]['career_job_title_1'],career_job_description_1:result[0]['career_job_description_1'],career_job_image_1:result[0]['career_job_image_1']});
+    }).catch(error=>{
+      this.setState({career_job_title_1:'???',career_job_description_1:'???',career_job_image_1:'???'})
+    })
+  }
   render() {
     return (
       <Fragment>
@@ -15,14 +32,10 @@ class PersonalTrainingCareer extends Component {
             </Col>
             <Col className='PersonalCarCol2' lg={6} md={6} sm={6}>
               <div className='PerCarT'>
-                  <h1 className='PerCarTitile'>PERSONAL TRAINING</h1>
+                  <h1 className='PerCarTitile'>{this.state.career_job_title_1}</h1>
               </div>
               <div className='PersonalCarDescription'>
-                  <p className='PerCarDes'>We offer multiple ways to impact the lives of our members.</p>
-                  <p className='PerCarDes'>Positions include: Sales Managers; Certified Trainers; Nutrition Advisors</p>
-                  <p className='PerCarDes'>Certified Trainers have a passion for fitness and helping others achieve their fitness goals.  Whether you are an experienced and certified trainer, or just a fitness junky that wants to become a trainer, we are the best place for your career.   All of our trainers enjoy respectable earning potential, full benefits, and continued skills development. If you are not certified already, that is just fine by us.  If we hire you, we will pay for your certifications.   Earning Potential:  $30k to $50k annually</p>
-                  <p className='PerCarDes'>Nutrition Advisors help our members and clients improve their eating to reach their goals and improve overall health.  Earning Potential: $30k to $60k annually</p>
-                  <p className='PerCarDes'>Trainer Sales Managers focus primarily on meeting with as many members to demonstrate the value of our Certified Trainers.  They find the right program at the right budget for the client then pair them with the right trainer.  Earning Potential: $45k to $75k annually</p>
+                  <p className='PerCarDes'>{this.state.career_job_description_1}</p>
               </div>
             </Col>
           </Row>
